aa888b116e4afd62053d74dd613cfc960294c003
Rafał Wójcik <rafal.wojcik.external@atos.net>
2016-05-04 10:29:42 +0200

    Add threaded keyboard input detection, couple of editors

--- Receiver/run_blocks.bat
+++ Receiver/run_blocks.bat
@@ -1,6 +1,6 @@
 start python video_streaming/ts_server.py -to 5005
 timeout 1
-start python video_streaming/block.py -from 5005 -to 5006 -editor FrameEditorEllipse
+start python video_streaming/block.py -from 5005 -to 5006 -editor FrameEditorCircles
 timeout 1
 start python video_streaming/ts_shower.py -from 5006
 PAUSE
--- Receiver/video_streaming/video_streaming_core.py
+++ Receiver/video_streaming/video_streaming_core.py
@@ -4,10 +4,13 @@ import socket
 from UserString import MutableString
 import cv2
 import numpy as np
+import threading, time
+from msvcrt import getch
+
+key = ''
 
 class FrameEditorEmpty:
     def frame_edit(self, frame):
-        #cv2.ellipse(frame,(256,256),(100,50),0,0,180,255,-1)
         return frame
 
 class FrameEditorEllipse:
@@ -15,11 +18,37 @@ class FrameEditorEllipse:
         cv2.ellipse(frame,(256,256),(100,50),0,0,180,255,-1)
         return frame
 
+class FrameEditorGreyscale:
+    def frame_edit(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return frame
+
+class FrameEditorSmoothing:
+    def frame_edit(self, frame):
+        frame = cv2.GaussianBlur(frame, (11, 11), 0)
+        return frame
+
+class FrameEditorDerivative:
+    def frame_edit(self, frame):
+        frame = cv2.Laplacian(frame, cv2.CV_64F)
+        return frame
+
+class FrameEditorCircles:
+    def frame_edit(self, frame):
+        gsFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(gsFrame, cv2.HOUGH_GRADIENT, dp=2, minDist=50, param1=100, param2=60, minRadius=50, maxRadius=0)
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            cv2.circle(img=frame,center=(i[0],i[1]),radius=i[2],color=(128,128,128,128),thickness=1)
+            cv2.circle(img=frame,center=(i[0],i[1]),radius=2,color=(64,64,64,128),thickness=1)
+        return frame
+
 
 class FrameSinkShower:
     def frame_sink(self, frame):
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            key = 'q'
             return False
         return True
 
@@ -45,11 +74,15 @@ class FrameSinkServer:
         self.conn.close()
 
     def frame_sink(self, frame):
-        retval, buf = cv2.imencode(".jpg", frame)
-        if not retval:
+        try:
+            retval, buf = cv2.imencode(".jpg", frame)
+            if not retval:
+                return False
+            self.conn.send("%d*" % (buf.size))
+            self.conn.send(buf)
+        except (socket.error, cv2.error) as e:
+            print str(e)
             return False
-        self.conn.send("%d*" % (buf.size))
-        self.conn.send(buf)
         return True
 
 def recv_data(number, s):
@@ -105,12 +138,25 @@ class CameraFrameGenearator:
         return
 
 def recive_and_sink_video(frameEditor, framesDst, framesSrc):
-    framesSrc.generator_init()
-    framesDst.sink_init()
+    threading.Thread(target = key_reader).start()
     while 1:
-        frame = framesSrc.gen_frame()
-        frame = frameEditor.frame_edit(frame)
-        if not framesDst.frame_sink(frame):
+        framesSrc.generator_init()
+        framesDst.sink_init()
+        while 1:
+            frame = framesSrc.gen_frame()
+            frame = frameEditor.frame_edit(frame)
+            if not framesDst.frame_sink(frame) or key == 'q':
+                break
+        framesDst.sink_finish()
+        framesSrc.generator_finish()
+        if key == 'q':
             break
-    framesDst.sink_finish()
-    framesSrc.generator_finish()
+
+def key_reader():
+    global key
+    lock = threading.Lock()
+    while True:
+        with lock:
+            key = getch()
+        if key == 'q':
+            break
