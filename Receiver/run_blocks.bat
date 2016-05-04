2cf98631a378aaefd290e64e3a63fef0432cff4a
Rafał Wójcik <rafal.wojcik.external@atos.net>
2016-05-04 11:54:21 +0200

    Online extreme programming

--- Receiver/run_blocks.bat
+++ Receiver/run_blocks.bat
@@ -2,5 +2,7 @@ start python video_streaming/ts_server.py -to 5005
 timeout 1
 start python video_streaming/block.py -from 5005 -to 5006 -editor FrameEditorCircles
 timeout 1
-start python video_streaming/ts_shower.py -from 5006
+start python video_streaming/block.py -from 5006 -to 5007 -editor FrameEditorSmoothing
+timeout 1
+start python video_streaming/ts_shower.py -from 5007
 PAUSE
--- Receiver/test.bat
+++ Receiver/test.bat
@@ -1,8 +1,10 @@
 start python ts_server.py -to 5005
-timeout 1
-start python block.py -from 5005 -to 5006 -editor FrameEditorSmoothing
-timeout 1
+timeout /t 1
+timeout /t 1start python block.py -from 5005 -to 5006 -editor FrameEditorSmoothing
+timeout /t 1
 start python block.py -from 5006 -to 5007 -editor FrameEditorCircles
-timeout 1
+timeout /t 1
 start python ts_shower.py -from 5007
-PAUSE
+
+
+exit
