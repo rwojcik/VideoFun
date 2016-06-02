98a473395509ad4b42f8a4705fcd01be4d45dc76
Rafał Wójcik <rafalwojcik3@gmail.com>
2016-06-02 19:55:15 +0200

    Cleanup python solution

--- Receiver/video_streaming/gen_cmd.bat
+++
@@ -1,5 +0,0 @@
-start python ts_server.py -to 5005
-start python ts_shower.py -from 5008,5009 -merge FrameMergerStack
-start python block.py -from 5005 -to 5006,5007  -editor FrameEditorResize  -editorparams 0.3000,0.5000
-start python block.py -from 5006 -to 5008  -editor FrameEditorSmoothing  -editorparams 11,11,0
-start python block.py -from 5007 -to 5009  -editor FrameEditorGreyscale
--- Receiver/video_streaming/test.bat
+++ Receiver/video_streaming/test.bat
@@ -1,9 +1,5 @@
 start python ts_server.py -to 5005,5006
-timeout 1
 start python block.py -from 5005 -to 5008 -editor FrameEditorSmoothing
-timeout 1
 start python block.py -from 5006 -to 5009 -editor FrameEditorCircles
-timeout 1
 start python block.py -from 5008,5009 -to 5010 -merge FrameMergerBlending -editor FrameEditorFramesCounter
-timeout 1
 start python ts_shower.py -from 5010
