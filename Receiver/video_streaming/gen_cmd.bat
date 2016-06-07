a32162a4b6324be95959af825fdc64bbdd54a7a5
Jacek Skoczylas <gjacek6@gmail.com>
2016-06-07 20:11:14 +0200

    co zrobic txt

--- Receiver/video_streaming/gen_cmd.bat
+++ Receiver/video_streaming/gen_cmd.bat
@@ -1,5 +1,4 @@
-start python ts_server.py -to 5005
-start python ts_shower.py -from 5008,5009 -merge FrameMergerStack
-start python block.py -from 5005 -to 5006,5007  -editor FrameEditorResize  -editorparams 0.3000,0.5000
-start python block.py -from 5006 -to 5008  -editor FrameEditorSmoothing  -editorparams 11,11,0
-start python block.py -from 5007 -to 5009  -editor FrameEditorGreyscale
+start python ts_server.py -to 5013
+start python ts_shower.py -from 5015,5016 -merge FrameMergerStack
+start python block.py -from 5013 -to 5014,5016  -editor FrameEditorResize  -editorparams 0.7000,0.7000
+start python block.py -from 5014 -to 5015  -editor FrameEditorSmoothing  -editorparams 11,11,0
